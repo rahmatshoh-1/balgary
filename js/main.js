b7a71849393bd52682c9ac3be354f462b0c78d24
unknown <rahmatshoh.rahmonov@mail.ru>
2022-07-27 10:07:10 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// $(function(){
+//     $('.tab').on('click', function(){
+//         // tab 
+//         $('.tab').removeClass('tab_active')
+//         $('.tab[data-tab='+$(this).attr('data-tab')+']').toggleClass('tab_active')
+       
+//         // content
+//         $('.content').removeClass('content_active')
+//         $('.content[data-tab='+$(this).attr('data-tab')+']').toggleClass('content_active') 
+       
+//     })
+// })
+
+$(function(){
+$('.button_more').on('click', function(){
+    $('#exampleModal').arcticmodal();
+
+})
+});
+
+
+
+
+
+
+
+
+
